a11ad85e976e181472c989751a16f6c6a8bd5c4f
Kim Slawson <kimslawson@gmail.com>
2014-03-27 18:16:04 -0400

    Add front end with placeholders.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-ninehundredforty
-================
+#Nine Hundred Forty
 
-There are only 940 Saturdays between your child's birth and them leaving for college. Spend them wisely.
+There are only 940 Saturdays between your child's birth and them leaving for college. In order to help you spend them wisely, explore these ideas. Please submit yours.
+
+##Background
+
+The inspiration for the site came from Dr. Harley Rotbart's http://www.noregretsparenting.com/
+
+##Implementation
+
+Nine Hundred Forty stores submissions first using the browser's local storage, then uploads submissions to the server for public access.
+
+##Questionable Content
+
+To report questionable or inappropriate content, please use the link provided next to the (in)appropriate image.
+
+##Shout-outs
+
+Nine Hundred Forty relies extensively on Hoodie to do such essential but mundane tasks as user authentication and data storage.
---
+++ ninehundredforty.js
@@ -0,0 +1,54 @@
+$('.dot').click(function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+//  alert($(this).parent().attr('class'));
+	$(this).parent().toggleClass('bigger');
+	if($(this).css('background-image') == 'none') {
+	//	var bkg = 'http://flickholdr.com/1000/1000/kids,family/' + $(this).parent().attr('id').replace('ordinal-','');
+		var bkg = 'http://pixelholdr.com/kids,vacation/1000x1000/seed:' + $(this).parent().attr('id').replace('ordinal-','') + ',dimensions:hide';
+	//	alert(bkg);
+		$(this).css('background-image', 'url(' + bkg + ')');
+	}
+});
+
+$('#add').click(function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	$('#upload').toggleClass('visible');
+});
+
+$('#what').click(function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	$('#description').toggle();
+});
+
+function dateToYMD(date) {
+    var d = date.getDate();
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
+    return '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d);
+}
+
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+}
+
+
+$(document).ready(function() {
+	var startDate = new Date();
+	startDate.setFullYear(2005, 2, 12); /* month is zero-indexed but not day? weird. */
+	var weekDate = new Date();
+	weekDate = startDate;
+
+	$('.dot-container').each(function(i){
+		var num = (i%4) + 1;
+		var ord = i + 1;
+		$(this).addClass('class-' + num).attr('id','ordinal-' + ord);
+		$(this).find('.dot').append('<span class="ordinal">' + ord + '</span>');
+		$(this).append('<span class="date">' + dateToYMD(weekDate) + '</span>');
+		weekDate = weekDate.addDays(7);
+	});
+});
